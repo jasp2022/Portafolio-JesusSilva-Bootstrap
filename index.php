0f268bf067bd496945214824ab97cc53206dc709
JS <jesusilpl@gmail.com>
2023-11-18 11:42:37 -0500

    section-profile

--- index.php
+++ index.php
@@ -3,13 +3,66 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Portafolio || Jesús Silva</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">  
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    
+ <!-- Navbar -->
+<div class="container">
+        <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand " href="#">Jesús Silva</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Experience</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Projects</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+ <!-- End-Navbar -->   
+
+
+  <!-- section profile -->
+  <section id="profile">
+      <div class="row">
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <div class="section-container">
+                <img class="img-fluid" src="assets/profile-pic.png" alt="">
+            </div>
+
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12">
+                <div class="header">
+                  <h1>Hi, I'm Jesús Silva</h1>
+                  <p>Full Stack Developer</p>
+                  <button type="button" class="btn btn-outline-dark">Download CV</button>
+              </div>
+              
+          </div>
+      </div>
+    </section>
+
+
+
+</div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    
   </body>
 </html>
